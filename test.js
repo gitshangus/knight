8647d5537121e1dd1c55df2e2df47f4ddb40005a
gitshangus <129347596+gitshangus@users.noreply.github.com>
2023-05-03 23:12:37 +0900

    230503

--- test.js
+++ test.js
@@ -6,9 +6,8 @@ var infoproc = {
   "정보시스템 구축관리":['소프트웨어 공학', '데이터베이스', '통신', '보안']
 };
 var $h3 = document.createElement('h3');
-$h3.innerText = 'yo';
 var $ul = document.createElement('ul');
-
+/*
 for (var sub in infoproc) {
   if (infoproc.hasOwnProperty(sub)) {
     var course = infoproc[sub];
@@ -17,5 +16,18 @@ for (var sub in infoproc) {
     $ul.appendChild($li);
   }
 }
+*/
 document.querySelector('.subject').appendChild($h3);
 document.querySelector('.subject').appendChild($ul);
+
+var subs = document.querySelectorAll('.subject');
+subs.forEach((sub, i) => {
+  var subname = Object.keys(infoproc)[i];
+  sub.querySelector('h3').textContent = subname;
+  var courses = infoproc[subname];
+  courses.forEach((course, i)  => {
+    var $li = document.createElement('li');
+    sub.querySelector('ul').appendChild($li);
+    $li.textContent = courses[i];
+  });
+});
