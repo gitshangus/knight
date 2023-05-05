247a792a1a78d2f97e214b4c721815083d8ce3f2
gitshangus <129347596+gitshangus@users.noreply.github.com>
2023-05-05 16:56:14 +0900

    230505 회사에서 수정

--- test.js
+++ test.js
@@ -6,13 +6,47 @@ var infoproc = {
   "정보시스템 구축관리":['소프트웨어 공학', '데이터베이스', '통신', '보안']
 };
 
-var subs = document.querySelectorAll('.subject');
-
 var category = function(){
-  var grids = Array.from(subs);
-  var $h3 = document.createElement('h3');
-  var $ul = document.createElement('ul');
-  for(grid in grids){
+  var subs = document.querySelectorAll('.subject');
+
+  console.log(subs);
+  console.log(Object.keys(subs)+' : '+Object.values(subs));
+  console.log(subs.length);
+  for(var i = 0; i < subs.length; i++){
+    console.log(subs[i]);
+  }
+  
+  for(var i = 0; i < subs.length; i++){
+    var $h3 = document.createElement('h3');
+    var $ul = document.createElement('ul');
+    var $li = document.createElement('li');
+    var subname = Object.keys(infoproc)[i];
+    subs[i].appendChild($h3);
+    subs[i].appendChild($ul); 
+    var subsul = subs[i].querySelector('ul');
+    subs[i].querySelector('h3').textContent = subname;
+    var courses = [];
+    courses.push(infoproc[subname]);
+    courses.forEach((course) => { 
+      console.log(course);
+      for(var i = 0; i < course.length; i++){
+        console.log(course[i]);
+        $li.innerText = course[i];
+        subsul.appendChild($li);
+      }    
+    });
+  }
+};
+
+category();
+
+
+/*
+var category = function(){
+  var subs = document.querySelectorAll('.subject');
+  for(sub in subs){
+    var $h3 = document.createElement('h3');
+    var $ul = document.createElement('ul');
     document.querySelector('.subject').appendChild($h3);
     document.querySelector('.subject').appendChild($ul);
     subs.forEach((sub, i) => {
@@ -29,3 +63,4 @@ var category = function(){
 };
 
 category();
+*/
