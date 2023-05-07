9af3cf0780e6a369e83b9809ad2e3e225bebcd43
gitshangus <129347596+gitshangus@users.noreply.github.com>
2023-05-07 15:42:54 +0900

    230507

--- main.js
+++ main.js
@@ -5,6 +5,16 @@ var infoproc = {
   "프로그래밍 언어 활용":['프로그래밍 언어론', '운영체제', '통신', '보안'],
   "정보시스템 구축관리":['소프트웨어 공학', '데이터베이스', '통신', '보안']
 };
+var links = {
+  '소프트웨어 공학': 'software_engineering.html',
+  '프로그래밍 언어론': 'http://www.example.com/programming_languages',
+  '자료구조': 'http://www.example.com/data_structures',
+  '운영체제': 'http://www.example.com/operating_systems',
+  '알고리즘': 'http://www.example.com/algorithms',
+  '보안': 'http://www.example.com/security',
+  '데이터베이스': 'http://www.example.com/database',
+  '통신': 'http://www.example.com/communication'
+};
 
 var category = function(){
   var subs = document.querySelectorAll('.subject');
@@ -17,12 +27,13 @@ var category = function(){
     var subsul = subs[i].querySelector('ul');
     subs[i].querySelector('h3').textContent = subname;
     var courses = infoproc[subname];
-    for(course of courses){
+    for(let course in courses){
       var $li = document.createElement('li');
-      $li.innerText = course;
+      var $a = document.createElement('a');
+      $a.setAttribute('href', links[courses[course]]);
+      $a.textContent = courses[course];
+      $li.appendChild($a);
       subsul.appendChild($li);
     }
   }
 };
-
-category();
